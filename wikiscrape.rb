b858943ff6c78c344e672e7410ed7ae8bfd1c1c6
Colin Gan <beshimi@hotmail.com>
2008-12-09 17:28:42 +0900

    Search...

--- wikiscrape.rb
+++ wikiscrape.rb
@@ -11,8 +11,8 @@ require 'open-uri'
 #raw = Hpricot(open('http://en.wikipedia.org/wiki/Swan_River_(Western_Australia)'))
 #raw = Hpricot(open('http://en.wikipedia.org/wiki/Inverloch_(webcomic)'))
 #raw = Hpricot(open('/home/colin/Paris'))
-raw = Hpricot(open('http://en.wikipedia.org/wiki/Inverloch'))
-#raw2 = Hpricot(open("/home/colin/wikipedia_test") {|x| x.to_s.slice(1..x.to_s.index('<table class="toc" id="toc" summary="Contents">'))})
+#raw = Hpricot(open('http://en.wikipedia.org/wiki/Inverloch'))
+raw = Hpricot(open('http://en.wikipedia.org/wiki/asjiajs'))
 #
 #
 #Chops off any content after Table of Contents
@@ -26,24 +26,24 @@ end
 disambig = (wiki/"div.dablink")
 bod = (wiki/"#bodyContent")
 
+search = (wiki/"ul.mv-search-results")
+
+no_article_found = (wiki/"div.noarticletext")
+unless no_article_found.empty?
+puts no_article_found.inner_text
+end
+
 puts disambig.inner_text if not disambig.empty?
 puts "\n"
 
-#links.each_child {|x| print x.inner_text}
-#links.inner_text.split(".").each {|x| puts x}
-#content = (bod/"p").inner_html.to_s
 content = (bod/"p")
-#content.gsub!(/<\/?[^>]*>/, "")
-#print content.inner_text
-#content = content.inner_text.gsub!(/\[[\w\d]*\]/, "")
-#puts content
+#
 unless content.inner_text.index(/(.*)* can refer to:/)  #do not print content if disambig
 content.inner_text.gsub!(/\[[\w\d]*\]/, "").each {|c| puts c}
 else
+  #prints disambig information
   puts content.inner_text
   puts "\n"
   links = (bod/"li")
   links.each {|link| puts link.inner_text} unless links.empty?
 end
-#content2 = content.split(".")
-#content2.each {|x| puts x}
